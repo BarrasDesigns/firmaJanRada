aaef7231dad646067b81e7e3b78254f7aed9c73b
jobarv <jbarrera@vabaja.com.mx>
2022-11-25 18:32:21 -0600

    display-block4

---
+++ js/main.js
@@ -0,0 +1,17 @@
+function show() {
+    var y = document.getElementById("showme");
+    if (y.style.display === "block") {
+        y.style.display = "none";
+    } else {
+        y.style.display = "block";
+    }
+
+}
+function hide() {
+    var x = document.getElementById("hideme");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
