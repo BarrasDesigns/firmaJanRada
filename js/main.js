dbedc57840162e7fea5c082164f59d5ebb8cb86f
jobarv <jbarrera@vabaja.com.mx>
2022-12-09 13:48:51 -0600

    prueba de click2

--- js/main.js
+++ js/main.js
@@ -7,13 +7,13 @@ function show() {
     }
 
 }
-// function hide() {
-//     const x = document.getElementById("hideme");
+function hide() {
+    const x = document.getElementById("hideme");
 
-//     if (x.style.display === "block") {
-//         x.style.display = "none";
-//     } else {
-//         x.style.display = "block";
-//     }
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
 
-// }
+}
