d7af4662c983d7cfdfe568c428ab7d2205de605c
jobarv <jbarrera@vabaja.com.mx>
2022-12-12 11:02:24 -0600

    prueba de click2

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
 function show() {
     const y = document.getElementById("showme");
-    if (y.style.display === "none") {
-        y.style.display = "block";
-    } else {
+    if (y.style.display === "block") {
         y.style.display = "none";
+    } else {
+        y.style.display = "block";
     }
 
 }
